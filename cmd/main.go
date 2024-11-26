1999a0b4e4b93d10c62a5beec67d1dc34951d83c
AmeerMoustafa <Ameer.moustafa1@gmail.com>
2024-11-27 01:54:23 +0200

    Refactored websocket server into controllers folder.

--- cmd/main.go
+++ cmd/main.go
@@ -1,69 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"net/http"
-	"thewire/internal/database"
+	websocketServer "thewire/controllers"
 
 	"golang.org/x/net/websocket"
 )
-
-type Server struct {
-	conns map[*websocket.Conn]bool
-}
 
 func servIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../templates/index.html")
 }
 
-func newServer() *Server {
-	return &Server{
-		conns: make(map[*websocket.Conn]bool),
-	}
-}
-
-func (s *Server) handleWS(ws *websocket.Conn) {
-	fmt.Println("New incoming connection from client:", ws.RemoteAddr())
-	s.conns[ws] = true
-	s.readLoop(ws)
-}
-
-func (s *Server) readLoop(ws *websocket.Conn) {
-	for {
-		var packet map[string]interface{}
-		websocket.JSON.Receive(ws, &packet)
-		if len(packet) > 0 {
-			message := string(packet["message_input"].(string))
-			formatted_message := fmt.Sprintf(`<div id="message" hx-swap-oob="beforeend">
-    	<p>%s</p></div>`, html.EscapeString(message))
-			go s.broadcast([]byte(formatted_message))
-		}
-
-	}
-
-}
-
-func (s *Server) broadcast(b []byte) {
-	for ws := range s.conns {
-		func(ws *websocket.Conn) {
-			if _, err := ws.Write(b); err != nil {
-				fmt.Println("Write Error", err)
-			}
-		}(ws)
-	}
+func servLogin(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../templates/login.html")
 
 }
 
 func main() {
-	server := newServer()
+	WSServer := websocketServer.NewServer()
 	http.HandleFunc("/", servIndex)
-	http.Handle("/ws", websocket.Handler(server.handleWS))
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../templates/login.html")
-
-	})
-	database.Connect()
-
+	http.Handle("/ws", websocket.Handler(WSServer.HandleWS))
+	http.HandleFunc("/login", servLogin)
 	http.ListenAndServe(":1337", nil)
 }
---
+++ controllers/websocketServer.go
@@ -0,0 +1,50 @@
+package websocketServer
+
+import (
+	"fmt"
+	"html"
+
+	"golang.org/x/net/websocket"
+)
+
+type WSServer struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *WSServer {
+	return &WSServer{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *WSServer) HandleWS(ws *websocket.Conn) {
+	fmt.Println("New incoming connection from client:", ws.RemoteAddr())
+	s.conns[ws] = true
+	s.readLoop(ws)
+}
+
+func (s *WSServer) readLoop(ws *websocket.Conn) {
+	for {
+		var packet map[string]interface{}
+		websocket.JSON.Receive(ws, &packet)
+		if len(packet) > 0 {
+			message := string(packet["message_input"].(string))
+			formatted_message := fmt.Sprintf(`<div id="message" hx-swap-oob="beforeend">
+    	<p>%s</p></div>`, html.EscapeString(message))
+			go s.broadcast([]byte(formatted_message))
+		}
+
+	}
+
+}
+
+func (s *WSServer) broadcast(b []byte) {
+	for ws := range s.conns {
+		func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("Write Error", err)
+			}
+		}(ws)
+	}
+
+}
